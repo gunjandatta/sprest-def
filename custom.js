94dc907e1b5d242c37f80d3dfef0c4332ad2d272
Gunjan Datta <me@dattabase.com>
2021-09-22 19:33:32 -0400

    Fixed the comments add input parameter.

--- custom.js
+++ custom.js
@@ -39,7 +39,7 @@ module.exports = {
         {
             name: "add",
             returnType: "Microsoft.SharePoint.Comments.comment",
-            params: [{ $: { Name: "text", Type: "Microsoft.SharePoint.Comments.comment" } }]
+            params: [{ $: { Name: "text", Type: "string" } }]
         }
     ],
 
